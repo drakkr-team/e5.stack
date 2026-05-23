40d17431462b6f706bf014bcfec96ac31f0aa755
elowanaud <elowan.audouin@icloud.com>
2026-05-23 17:43:13 +0200

    feat(web): create profile related mutation

---
+++ apps/web/src/features/user_management/profile/hooks/use-delete-mutation.ts
@@ -0,0 +1,43 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "@tanstack/react-router";
+import { toast } from "@workspace/ui-react/components/toast";
+import { useTranslation } from "react-i18next";
+import { api } from "#/libs/tuyau";
+import { toastifyTuyauError } from "#/utils/tuyau";
+
+export function useDeleteProfileMutation() {
+	const { t } = useTranslation("features.user_management.profile.hooks.use-delete-mutation");
+
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
+
+	return useMutation(
+		api.profile.delete.mutationOptions({
+			onSuccess: () => {
+				toast.success(t("success.title"), {
+					description: t("success.description"),
+				});
+				queryClient.removeQueries({
+					queryKey: api.profile.view.pathKey(),
+				});
+				navigate({ to: "/login" });
+			},
+			onError: (error) => {
+				toastifyTuyauError(error, {
+					E_NETWORK: [
+						t("error.E_NETWORK.title"),
+						{ description: t("error.E_NETWORK.description") },
+					],
+					E_UNAUTHENTICATED: [
+						t("error.E_UNAUTHENTICATED.title"),
+						{ description: t("error.E_UNAUTHENTICATED.description") },
+					],
+					E_UNEXPECTED: [
+						t("error.E_UNEXPECTED.title"),
+						{ description: t("error.E_UNEXPECTED.description") },
+					],
+				});
+			},
+		}),
+	);
+}
